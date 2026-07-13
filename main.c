52e6fae07c840dcbfd19c7e20157e46243cbfdc6
Mahmud Syakiran <kiran@dmostechnologies.com>
2026-07-13 20:52:23 +0800

    Code Base Pattern 1

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+void pattern1()
+{
+	for (int j = 1; j <= 4; j++)
+	{
+		for (int i = 1; i <= j; i++)
+		{
+			printf("%d", i);
+		}
+
+		printf("\n");
+	}
+}
+
 int main()
 {
-	printf("Hello World");
+	pattern1();
 
 	return 0;
 }
